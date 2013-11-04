89f441441c183245c4b35d5f3daff8b054f240a0
Leandro Soriano Ferreira <sfohart@gmail.com>
2013-11-04 19:09:17 -0300

    tentando montar o layout da parte web

---
+++ pesquisa-orientada/mestrado-web/src/main/java/br/com/ufba/mestrado/computacao/service/basic/ProjectSearchService.java
@@ -0,0 +1,7 @@
+package br.com.ufba.mestrado.computacao.service.basic;
+
+import java.io.Serializable;
+
+public interface ProjectSearchService extends Serializable {
+
+}
---
+++ pesquisa-orientada/mestrado-web/src/main/java/br/com/ufba/mestrado/computacao/web/MainManageBean.java
@@ -0,0 +1,10 @@
+package br.com.ufba.mestrado.computacao.web;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="mainMB")
+public class MainManageBean {
+
+	
+	
+}
