3ffa79758f4ee1022c6aa2fcc1e4e76f26fe2018
Leandro Soriano Ferreira <sfohart@gmail.com>
2013-11-13 18:20:43 -0300

    Construindo tela de review. Consertando componente de rating (estrelas)

--- pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/managedbean/ProjectReviewManagedBean.java
+++ pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/managedbean/ProjectReviewManagedBean.java
@@ -1,15 +1,42 @@
 package br.ufba.dcc.mestrado.computacao.web.managedbean;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ComponentSystemEvent;
 
 import br.ufba.dcc.mestrado.computacao.entities.ohloh.project.OhLohProjectEntity;
+import br.ufba.dcc.mestrado.computacao.entities.recommender.criterium.RecommenderCriteriumEntity;
+import br.ufba.dcc.mestrado.computacao.entities.recommender.preference.PreferenceEntity;
+import br.ufba.dcc.mestrado.computacao.entities.recommender.preference.PreferenceEntryEntity;
+import br.ufba.dcc.mestrado.computacao.service.base.OhLohProjectService;
+import br.ufba.dcc.mestrado.computacao.service.base.RecommenderCriteriumService;
 
 @ManagedBean(name="projectReviewMB", eager=true)
 @ViewScoped
-public class ProjectReviewManagedBean {
+public class ProjectReviewManagedBean implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3583610472886856317L;
 	
 	private OhLohProjectEntity project;
+	private PreferenceEntity preference;
+	
+	@ManagedProperty("#{ohLohProjectService}")
+	private OhLohProjectService projectService;
+	
+	@ManagedProperty("#{recommenderCriteriumService}")
+	private RecommenderCriteriumService criteriumService;
+	
+	public ProjectReviewManagedBean() {
+		this.project = new OhLohProjectEntity();
+	}
 	
 	public OhLohProjectEntity getProject() {
 		return project;
@@ -17,6 +44,52 @@ public class ProjectReviewManagedBean {
 	
 	public void setProject(OhLohProjectEntity project) {
 		this.project = project;
+	}
+
+	public OhLohProjectService getProjectService() {
+		return projectService;
+	}
+
+	public void setProjectService(OhLohProjectService projectService) {
+		this.projectService = projectService;
+	}
+	
+	public RecommenderCriteriumService getCriteriumService() {
+		return criteriumService;
+	}
+
+	public void setCriteriumService(RecommenderCriteriumService criteriumService) {
+		this.criteriumService = criteriumService;
+	}
+	
+	public PreferenceEntity getPreference() {
+		return preference;
+	}
+	
+	public void setPreference(PreferenceEntity preference) {
+		this.preference = preference;
+	}
+
+	public void init(ComponentSystemEvent event) {
+		if (getProject() != null && getProject().getId() != null) {
+			this.project = getProjectService().findById(getProject().getId());
+			
+			List<RecommenderCriteriumEntity> criteriumList = getCriteriumService().findAll();
+			if (criteriumList != null) {
+				this.preference = new PreferenceEntity();
+				
+				List<PreferenceEntryEntity> preferenceEntryEntityList = new ArrayList<PreferenceEntryEntity>();
+				for (RecommenderCriteriumEntity criterium : criteriumList) {
+					PreferenceEntryEntity preferenceEntry = new PreferenceEntryEntity();
+					preferenceEntry.setCriterium(criterium);
+					preferenceEntry.setCriteriumId(criterium.getId());
+					
+					preferenceEntryEntityList.add(preferenceEntry);
+				}
+				
+				this.preference.setPreferenceEntryList(preferenceEntryEntityList);
+			}
+		}
 	}
 
 }
