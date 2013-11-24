e91e763d6f40c6e8880da68f923ce91e160c578e
Leandro Soriano Ferreira <sfohart@gmail.com>
2013-11-24 12:07:15 -0300

    formatações e inicializações na tela de reviews

--- pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/managedbean/ProjectReviewManagedBean.java
+++ pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/managedbean/ProjectReviewManagedBean.java
@@ -9,6 +9,7 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ComponentSystemEvent;
 
+import br.ufba.dcc.mestrado.computacao.entities.ohloh.account.OhLohAccountEntity;
 import br.ufba.dcc.mestrado.computacao.entities.ohloh.project.OhLohProjectEntity;
 import br.ufba.dcc.mestrado.computacao.entities.recommender.criterium.RecommenderCriteriumEntity;
 import br.ufba.dcc.mestrado.computacao.entities.recommender.preference.PreferenceEntity;
@@ -16,6 +17,7 @@ import br.ufba.dcc.mestrado.computacao.entities.recommender.preference.Preferenc
 import br.ufba.dcc.mestrado.computacao.entities.recommender.preference.PreferenceReviewEntity;
 import br.ufba.dcc.mestrado.computacao.service.base.OhLohProjectService;
 import br.ufba.dcc.mestrado.computacao.service.base.RecommenderCriteriumService;
+import br.ufba.dcc.mestrado.computacao.service.basic.RepositoryBasedUserDetailsService;
 
 @ManagedBean(name="projectReviewMB", eager=true)
 @ViewScoped
@@ -27,6 +29,8 @@ public class ProjectReviewManagedBean implements Serializable {
 	private static final long serialVersionUID = -3583610472886856317L;
 	
 	private OhLohProjectEntity project;
+	private OhLohAccountEntity account;
+	
 	private PreferenceEntity preference;
 	
 	@ManagedProperty("#{ohLohProjectService}")
@@ -34,6 +38,9 @@ public class ProjectReviewManagedBean implements Serializable {
 	
 	@ManagedProperty("#{recommenderCriteriumService}")
 	private RecommenderCriteriumService criteriumService;
+	
+	@ManagedProperty("#{repositoryBasedUserDetailsService}")
+	private RepositoryBasedUserDetailsService userDetailsService;
 	
 	public ProjectReviewManagedBean() {
 		this.project = new OhLohProjectEntity();
@@ -45,6 +52,14 @@ public class ProjectReviewManagedBean implements Serializable {
 	
 	public void setProject(OhLohProjectEntity project) {
 		this.project = project;
+	}
+	
+	public OhLohAccountEntity getAccount() {
+		return account;
+	}
+	
+	public void setAccount(OhLohAccountEntity account) {
+		this.account = account;
 	}
 
 	public OhLohProjectService getProjectService() {
@@ -63,6 +78,15 @@ public class ProjectReviewManagedBean implements Serializable {
 		this.criteriumService = criteriumService;
 	}
 	
+	public RepositoryBasedUserDetailsService getUserDetailsService() {
+		return userDetailsService;
+	}
+	
+	public void setUserDetailsService(
+			RepositoryBasedUserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+	
 	public PreferenceEntity getPreference() {
 		return preference;
 	}
@@ -74,17 +98,29 @@ public class ProjectReviewManagedBean implements Serializable {
 	public void init(ComponentSystemEvent event) {
 		if (getProject() != null && getProject().getId() != null) {
 			this.project = getProjectService().findById(getProject().getId());
+			this.account = getUserDetailsService().loadFullLoggedUser();
 			
 			List<RecommenderCriteriumEntity> criteriumList = getCriteriumService().findAll();
 			if (criteriumList != null) {
 				this.preference = new PreferenceEntity();
-				this.preference.setPreferenceReview(new PreferenceReviewEntity());
+				
+				this.preference.setAccount(getAccount());
+				this.preference.setAccountId(getAccount().getId());
+				
+				this.preference.setProject(getProject());
+				this.preference.setProjectId(getProject().getId());
+				
+				PreferenceReviewEntity preferenceReview = new PreferenceReviewEntity();
+				preferenceReview.setPreference(this.preference);
+				
+				this.preference.setPreferenceReview(preferenceReview);
 				
 				List<PreferenceEntryEntity> preferenceEntryEntityList = new ArrayList<PreferenceEntryEntity>();
 				for (RecommenderCriteriumEntity criterium : criteriumList) {
 					PreferenceEntryEntity preferenceEntry = new PreferenceEntryEntity();
 					preferenceEntry.setCriterium(criterium);
 					preferenceEntry.setCriteriumId(criterium.getId());
+					preferenceEntry.setPreference(this.preference);
 					
 					preferenceEntryEntityList.add(preferenceEntry);
 				}
