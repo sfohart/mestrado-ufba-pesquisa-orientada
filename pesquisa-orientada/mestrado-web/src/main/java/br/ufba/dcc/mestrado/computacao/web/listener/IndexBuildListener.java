a0c8a94a91922c8b7ad9a32fcfa407055b498f4e
sfohart <sfohart@gmail.com>
2013-12-18 15:59:00 -0300

    listener para recriar o índice sempre que a aplicação for reinicializada

---
+++ pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/listener/IndexBuildListener.java
@@ -0,0 +1,55 @@
+package br.ufba.dcc.mestrado.computacao.web.listener;
+
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.ufba.dcc.mestrado.computacao.service.base.Indexer;
+
+/**
+ * Application Lifecycle Listener implementation class IndexListener
+ *
+ */
+@Component
+public class IndexBuildListener implements ServletContextListener {
+	
+	private static final Logger logger = Logger.getLogger(IndexBuildListener.class.getName());
+	
+	@Autowired
+	private Indexer indexer;
+
+    /**
+     * Default constructor. 
+     */
+    public IndexBuildListener() {
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent event) {
+    	//aplicação (re)inicializada. (Re)criar o índice
+    	try {
+    		logger.info("(Re)criando o índice do Apache Lucene/Hibernate Search");
+			indexer.buildIndex();
+			logger.info("Índice (re)criado");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent event) {
+        // TODO Auto-generated method stub
+    }
+	
+}
