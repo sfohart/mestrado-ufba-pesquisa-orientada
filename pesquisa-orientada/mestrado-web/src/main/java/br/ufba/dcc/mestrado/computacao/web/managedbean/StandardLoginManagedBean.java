7d9c66de9be3087befe6d653861408e575835c3f
sfohart <sfohart@gmail.com>
2014-03-29 07:01:10 -0300

    habilitando funcionalidade de filtrar recomendações por tags de interesse

--- pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/managedbean/StandardLoginManagedBean.java
+++ pesquisa-orientada/mestrado-web/src/main/java/br/ufba/dcc/mestrado/computacao/web/managedbean/StandardLoginManagedBean.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ComponentSystemEvent;
@@ -25,7 +25,7 @@ import br.ufba.dcc.mestrado.computacao.service.base.UserService;
 import br.ufba.dcc.mestrado.computacao.service.basic.RepositoryBasedUserDetailsService;
 
 @ManagedBean(name="loginMB")
-@SessionScoped
+@ViewScoped
 public class StandardLoginManagedBean implements Serializable {
 	
 	/**
