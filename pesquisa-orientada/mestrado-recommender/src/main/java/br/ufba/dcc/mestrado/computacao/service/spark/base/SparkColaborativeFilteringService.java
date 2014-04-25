8918e4a05ae3d0add107438edc223f16ab94df95
sfohart <sfohart@gmail.com>
2014-04-25 07:48:18 -0300

    mahout e spark conflitam

--- pesquisa-orientada/mestrado-recommender/src/main/java/br/ufba/dcc/mestrado/computacao/service/spark/base/SparkColaborativeFilteringService.java
+++
@@ -1,8 +0,0 @@
-package br.ufba.dcc.mestrado.computacao.service.spark.base;
-
-import br.ufba.dcc.mestrado.computacao.service.core.base.BaseColaborativeFilteringService;
-
-public interface SparkColaborativeFilteringService extends BaseColaborativeFilteringService {
-	
-
-}
